52cd08558e8672b5795ee5533985f3fb5c42c08a
Thomas Cellerier <tcellerier@users.noreply.github.com>
2018-05-13 09:56:45 +0200

    update for new operator Smovengo

--- velib.sql
+++ velib.sql
@@ -1,11 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 4.3.0
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Jul 14, 2015 at 06:31 PM
--- Server version: 5.5.43-MariaDB
--- PHP Version: 5.5.26
+-- version 4.6.6deb4
+-- https://www.phpmyadmin.net/
+-- Generation Time: Feb 10, 2018 at 03:32 PM
+-- Server version: 10.1.23-MariaDB-9+deb9u1
+-- PHP Version: 7.0.27-0+deb9u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,11 +12,13 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `velib`
 --
+CREATE DATABASE IF NOT EXISTS `velib` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `velib`;
 
 -- --------------------------------------------------------
 
@@ -26,17 +26,15 @@ SET time_zone = "+00:00";
 -- Table structure for table `stations_info`
 --
 
-CREATE TABLE IF NOT EXISTS `stations_info` (
-  `number` mediumint(9) unsigned NOT NULL,
-  `contract_name` varchar(50) NOT NULL,
+CREATE TABLE `stations_info` (
+  `code` varchar(6) NOT NULL,
+  `gps_lat` double NOT NULL,
+  `gps_lng` double NOT NULL,
+  `state` varchar(30) NOT NULL,
   `name` varchar(250) NOT NULL,
   `address` varchar(250) NOT NULL,
-  `position_lat` double NOT NULL,
-  `position_lng` double NOT NULL,
-  `banking` varchar(10) NOT NULL,
-  `bonus` varchar(10) NOT NULL,
-  `bike_stands` tinyint(3) unsigned NOT NULL,
-  `status` varchar(15) NOT NULL
+  `nbDock` tinyint(3) UNSIGNED NOT NULL,
+  `nbEDock` tinyint(3) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -45,11 +43,12 @@ CREATE TABLE IF NOT EXISTS `stations_info` (
 -- Table structure for table `stations_usage`
 --
 
-CREATE TABLE IF NOT EXISTS `stations_usage` (
-  `number` smallint(5) unsigned NOT NULL,
+CREATE TABLE `stations_usage` (
+  `code` varchar(6) NOT NULL,
   `last_update` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `available_bike_stands` tinyint(3) unsigned NOT NULL,
-  `available_bikes` tinyint(3) unsigned NOT NULL
+  `nbBike` tinyint(3) UNSIGNED NOT NULL,
+  `nbEbike` tinyint(3) UNSIGNED NOT NULL,
+  `nbFreeDock` tinyint(3) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -60,13 +59,13 @@ CREATE TABLE IF NOT EXISTS `stations_usage` (
 -- Indexes for table `stations_info`
 --
 ALTER TABLE `stations_info`
- ADD PRIMARY KEY (`number`,`contract_name`);
+  ADD PRIMARY KEY (`code`);
 
 --
 -- Indexes for table `stations_usage`
 --
 ALTER TABLE `stations_usage`
- ADD PRIMARY KEY (`number`,`last_update`);
+  ADD PRIMARY KEY (`code`,`last_update`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
