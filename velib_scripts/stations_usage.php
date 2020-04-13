5abbd7c6cc23ebfdfade9b0e42d526482a08f113
Thomas Cellerier <tcellerier@users.noreply.github.com>
2020-04-13 15:07:37 +0200

    update for official API

--- velib_scripts/stations_usage.php
+++ velib_scripts/stations_usage.php
@@ -9,12 +9,10 @@ $start_time = microtime(true);
 $timestamp_now = date("Y-m-d H:i:00");
 
 // Appel à l'API Velib
-$api_call = file_get_contents("https://www.velib-metropole.fr/webapi/map/details?gpsTopLatitude=50&gpsTopLongitude=5&gpsBotLatitude=40&gpsBotLongitude=2&zoomLevel=15");
-
+$api_call = file_get_contents("https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_status.json");
 
 // Décodage JSON
 $obj = json_decode($api_call);
-
 
 require_once '/home/pi/www/velib/mysql_login.php';
 $conn = new mysqli($hn, $un, $pw, $db);
@@ -23,23 +21,22 @@ if ($conn->connect_error) die($conn->connect_error);
 $i_ok = 0;
 $i_ko = 0;
 
-foreach($obj as $obj_i)
+foreach($obj->{'data'}->{'stations'}  as $obj_i)
 {
 
-	$station = $obj_i->{'station'};
-	$gps = $station->{'gps'};
-
-	$nb_velib = $obj_i->{'nbBike'}; // + $obj_i->{'nbBikeOverflow'}
-	$nb_velib_electrique = $obj_i->{'nbEbike'}; // + $obj_i->{'nbEBikeOverflow'} 
-	$nb_place_dispo = $obj_i->{'nbFreeDock'} + $obj_i->{'nbFreeEDock'};
+	//print_r($obj_i);
+	$stationcode = $obj_i->{'stationCode'};
+	$num_docks_available = $obj_i->{'numDocksAvailable'};
+	$num_bikes_mechanical = $obj_i->{'num_bikes_available_types'}[0]->{'mechanical'};
+	$num_bikes_ebike = $obj_i->{'num_bikes_available_types'}[1]->{'ebike'};
 
 	// Last update aroundi à la minute inférieur
-	$query = "INSERT INTO stations_usage VALUES (\"".$station->{'code'}."\", \"".$timestamp_now."\", ".$nb_velib.", ".$nb_velib_electrique.", ".$nb_place_dispo.")";
+	$query = "INSERT INTO stations_usage(stationcode, last_update, num_bikes_mechanical, num_bikes_ebike, num_docks_available) VALUES (\"".$stationcode."\", \"".$timestamp_now."\", ".$num_bikes_mechanical.", ".$num_bikes_ebike.", ".$num_docks_available.")";
 	//echo $query;
 	$result = $conn->query($query);
 	if (!$result) 
 	{
-		echo "Database access failed for station ". $station->{'code'}. " : " . $conn->error. " \n";
+		echo "Database access failed for station ". $stationcode. " : " . $conn->error. " \n";
 		$i_ko++;
 	}
 	else
@@ -48,6 +45,8 @@ foreach($obj as $obj_i)
 
 echo "\nNouvelles données pour " . $i_ok ." stations \n";
 echo "Données non mises à jour pour " . $i_ko ." stations\n";
+
+
 
 $end_time = microtime(true);
 $creationtime = ($end_time - $start_time);
